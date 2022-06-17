4405cbb3bc2351b05730586472435a73e8aa6053
althaf18 <107619554+althaf18@users.noreply.github.com>
2022-06-17 11:47:14 +0530

    Add files via upload

---
+++ proj1.java
@@ -0,0 +1,8 @@
+public class proj1  {
+    public static void main(String[] args)
+    {
+        System.out.println("hello world");
+        System.out.println("hello world");
+        System.out.println("hello world");
+    }
+}
