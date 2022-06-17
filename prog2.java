9a2a503f64c919cd5c5bdc4a3cd9b741e6cc8d30
althaf18 <107619554+althaf18@users.noreply.github.com>
2022-06-17 11:47:40 +0530

    Add files via upload

---
+++ prog2.java
@@ -0,0 +1,15 @@
+public class prog2{
+
+    public static void main(String[] args) {
+        int x=10,y=20,z=30;
+        if(x>=y&&x>=z)
+            System.out.println("x+ is the largest number");
+        else if (y>=x&&y>=z)
+            System.out.println("y+ is the largest number");
+        else
+            System.out.println("z+ is the gretest number");
+        }
+        }
+
+
+
